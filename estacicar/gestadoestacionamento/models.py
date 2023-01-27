c347a7396334ff1dde714814eb9c059dba1c763e
Eder Dias do Nascimento <ederptga@gmail.com>
2023-01-27 18:06:47 -0300

    configurando as models

--- estacicar/gestadoestacionamento/models.py
+++ estacicar/gestadoestacionamento/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100, )
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=10)
 
-print("vida")
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca)
+
