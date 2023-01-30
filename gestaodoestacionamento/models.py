d5f195c1562d8747ad340af430768c7545fc8931
Eder Dias do Nascimento <ederptga@gmail.com>
2023-01-30 12:24:50 -0300

    modelando programa

--- gestaodoestacionamento/models.py
+++ gestaodoestacionamento/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=200)
+    Telefone = models.CharField(max_length=200)
+    
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete= models.CASCADE)
+    placa = models.CharField(max_length=6)
+    cor = models.CharField(max_length=6)
+    observacao = models.TextField()
+
+
+
+
+
 # Create your models here.
