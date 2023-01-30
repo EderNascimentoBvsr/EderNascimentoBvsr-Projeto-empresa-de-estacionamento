68f555cca7acaa5a987c7ef25f03253d18fe250a
Eder Dias do Nascimento <ederptga@gmail.com>
2023-01-30 14:18:33 -0300

    Mudanças

--- gestaodoestacionamento/models.py
+++ gestaodoestacionamento/models.py
@@ -4,19 +4,26 @@ class Pessoa(models.Model):
     nome = models.CharField(max_length=50)
     endereco = models.CharField(max_length=200)
     Telefone = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
     
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
 
     def __str__(self):
-        return nome
+        return self.nome
 
 class Veiculo(models.Model):
     marca = models.ForeignKey(Marca, on_delete= models.CASCADE)
     placa = models.CharField(max_length=6)
+    proprietario = models.ForeignKey(Pessoa, on_delete= models.CASCADE)
     cor = models.CharField(max_length=6)
     observacao = models.TextField()
+
+    def __str__(self):
+        return self.marca + " - " + self.placa
 
 
 
