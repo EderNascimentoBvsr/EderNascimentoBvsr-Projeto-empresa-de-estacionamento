9e8eb5c5584a00aee3113abe50ae8d2d94d7ba8f
Eder Dias do Nascimento <ederptga@gmail.com>
2023-01-30 10:47:07 -0300

    atualização app

--- estacicar/gestadoestacionamento/admin.py
+++ estacicar/gestadoestacionamento/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import Pessoa, Marca, Veiculo
 
+admin.site.register(Pessoa)
+admin.site.register(Marca)
+admin.site.register(Veiculo)
 
-# Register your models here.
